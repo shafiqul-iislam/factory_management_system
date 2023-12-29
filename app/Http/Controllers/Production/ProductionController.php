3b71ab9cd84a85994815b2355dc92b4cefd4718b
shafiqul-iislam <shafiqulislam78652@gmail.com>
2023-12-29 12:52:53 +0600

    add production module

---
+++ app/Http/Controllers/Production/ProductionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\AdminPortal\Production;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductionController extends Controller
+{
+    //
+}
---
+++ database/migrations/2023_12_29_062736_create_productions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('department_id')->nullable();
+            $table->integer('product_id')->nullable();
+            $table->string('product_category')->nullable();
+            $table->string('production_target')->nullable();
+            $table->string('total_production')->nullable();
+            $table->integer('supervisor_id')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+
+
+            // product delivey module
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productions');
+    }
+};
