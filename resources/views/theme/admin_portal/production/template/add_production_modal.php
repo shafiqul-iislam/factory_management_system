8134888bbbf9a9fcf9af9c911aef637edfce8817
shafiqul-iislam <shafiqulislam78652@gmail.com>
2024-06-15 23:40:29 +0600

    on going

--- resources/views/theme/admin_portal/production/template/add_production_modal.php
+++ resources/views/theme/admin_portal/production/template/add_production_modal.php
@@ -10,17 +10,10 @@
                     </button>
                 </div>
                 <div class="modal-body">
-
-                    <div class="d-flex justify-content-center fixed-top mt-20">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="5 5 200 200" width="80" height="80" class="loader" style="display: none;">
-                            <linearGradient id="a11">
-                                <stop offset="0" stop-color="#FF156D" stop-opacity="0"></stop>
-                                <stop offset="1" stop-color="#FF156D"></stop>
-                            </linearGradient>
-                            <circle fill="none" stroke="url(#a11)" stroke-width="15" stroke-linecap="round" stroke-dasharray="0 44 0 44 0 44 0 44 0 360" cx="100" cy="100" r="70" transform-origin="center">
-                                <animateTransform type="rotate" attributeName="transform" calcMode="discrete" dur="2" values="360;324;288;252;216;180;144;108;72;36" repeatCount="indefinite"></animateTransform>
-                            </circle>
-                        </svg>
+                    <div class="d-flex justify-content-center">
+                        <div class="positon-static loading" style="display: none;">
+                            <img src="<?php echo asset('theme/assets/img/theme_img/loader/loading.svg'); ?>" alt="" width="100px" height="100px">
+                        </div>
                     </div>
 
                     <div class="row d-flex justify-content-center align-items-center mb-3">
@@ -137,12 +130,11 @@
                     console.log(error);
                 },
                 beforeSend: function() {
-                    $('.loader').show(500);
-                },
-                complete: function() {
-                    $('.loader').hide(500);
+                    $('.loading').fadeIn();
                 },
                 success: function(data) {
+                    $('.loading').fadeOut();
+
                     productSelector.html('');
                     productSelector.prepend(data.productOptions);
                     productSelector.addClass('border-success');
@@ -150,7 +142,6 @@
                     supervisorSelector.html('');
                     supervisorSelector.prepend(data.employeeOptions);
                     supervisorSelector.addClass('border-success');
-
                 },
             });
         });
