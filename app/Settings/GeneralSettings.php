ec711bdf378d69a7c9b6a87c17d64d20a156f424
shafiqul-iislam <shafiqulislam78652@gmail.com>
2024-08-29 09:25:25 +0600

    add spatie laravel settings

---
+++ app/Settings/GeneralSettings.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class GeneralSettings extends Settings
+{
+
+    public static function group(): string
+    {
+        return 'general';
+    }
+}
